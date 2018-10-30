d0d945b7844a589230e52c6bb277244a67e1dab0
vimux <vimux@protonmail.com>
2018-10-30 07:25:25 -0400

    Refactoring menu.js

--- static/js/menu.js
+++ static/js/menu.js
@@ -2,13 +2,10 @@ var toggle = document.getElementById('toggle');
 var menu = document.getElementById('menu');
 
 toggle.addEventListener('click', function () {
-	if (menu.classList.contains('main-nav__list--active')) {
-		this.setAttribute('aria-expanded', 'false');
-		this.classList.remove('main-nav__btn--active');
-		menu.classList.remove('main-nav__list--active');
-	} else {
-		menu.classList.add('main-nav__list--active');
-		this.classList.add('main-nav__btn--active');
-		this.setAttribute('aria-expanded', 'true');
-	}
+	menu.classList.toggle('main-nav__list--active');
+	this.classList.toggle('main-nav__btn--active');
+	this.setAttribute(
+		'aria-expanded',
+		this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+	);
 });
